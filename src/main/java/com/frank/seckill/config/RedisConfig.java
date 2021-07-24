c10b6c92118f0ee36d05c3c96dec25781ae40eb7
MRTFrank <1428151366@qq.com>
2021-07-24 11:12:58 +0800

    fix redis分布式锁

--- src/main/java/com/frank/seckill/config/RedisConfig.java
+++ src/main/java/com/frank/seckill/config/RedisConfig.java
@@ -1,11 +1,15 @@
 package com.frank.seckill.config;
 
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultReactiveScriptExecutor;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
@@ -29,6 +33,16 @@ public class RedisConfig {
         //注入连接工厂
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
+    }
+
+    @Bean
+    public DefaultRedisScript<Boolean> script(){
+        DefaultRedisScript<Boolean> redisScript = new DefaultRedisScript<>();
+        //lock.lua脚本位置和application.yml同级目录
+        redisScript.setLocation(new ClassPathResource("lock.lua"));
+        redisScript.setResultType(Boolean.class);
+
+        return redisScript;
     }
 
 
--- src/test/java/com/frank/seckill/SeckillApplicationTests.java
+++ src/test/java/com/frank/seckill/SeckillApplicationTests.java
@@ -1,9 +1,16 @@
 package com.frank.seckill;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.script.RedisScript;
+
+import java.util.Collections;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class SeckillApplicationTests {
@@ -11,9 +18,43 @@ class SeckillApplicationTests {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @Autowired
+    private RedisScript<Boolean> redisScript;
+
     @Test
     public void testLock01() {
+        //如果key不存在才可以设置成功
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        Boolean isLock = valueOperations.setIfAbsent("k1", "v1");
+        //如果占位成功，进行操作
+        if(isLock){
+            valueOperations.set("name","xxxx");
+            String name = (String) valueOperations.get("name");
+            System.out.println("name = "+name);
+            //操作结束，释放锁
+            redisTemplate.delete("k1");
+        }else{
+            System.out.println("线程忙");
+        }
+    }
 
+    @Test
+    public void testLock03() {
+        //如果key不存在才可以设置成功
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        String s = UUID.randomUUID().toString();
+        Boolean isLock = valueOperations.setIfAbsent("k1", s,120, TimeUnit.SECONDS);
+        //如果占位成功，进行操作
+        if(isLock){
+            valueOperations.set("name","xxxx");
+            String name = (String) valueOperations.get("name");
+            System.out.println("name = "+name);
+            System.out.println(valueOperations.get("k1"));
+            Boolean result =(Boolean) redisTemplate.execute(redisScript, Collections.singletonList("k1"), s);
+            System.out.println(result);
+        }else{
+            System.out.println("线程忙");
+        }
     }
 
 }
